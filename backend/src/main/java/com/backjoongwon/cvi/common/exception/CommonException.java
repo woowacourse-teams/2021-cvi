19f5f413efcaeaa051f4983a6b5e7278265aa689
TaeJeong Kim <xowjd41@naver.com>
2021-07-08 11:56:05 +0900

    feat: 공통 exception 정의 및 RestControllerAdvice 적용

---
+++ backend/src/main/java/com/backjoongwon/cvi/common/exception/CommonException.java
@@ -0,0 +1,8 @@
+package com.backjoongwon.cvi.common.exception;
+
+public class CommonException extends RuntimeException {
+
+    public CommonException(String message) {
+        super(message);
+    }
+}
---
+++ backend/src/main/java/com/backjoongwon/cvi/common/exception/InvalidInputException.java
@@ -0,0 +1,8 @@
+package com.backjoongwon.cvi.common.exception;
+
+public class InvalidInputException extends CommonException {
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
