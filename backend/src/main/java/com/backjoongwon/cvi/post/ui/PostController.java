18400651109631635827a23d298b1637ca208137
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-07-08 14:44:58 +0900

    feat: 글 등록 문서화 추가

--- backend/src/main/java/com/backjoongwon/cvi/post/ui/PostController.java
+++ backend/src/main/java/com/backjoongwon/cvi/post/ui/PostController.java
@@ -4,12 +4,15 @@ import com.backjoongwon.cvi.post.dto.PostRequest;
 import com.backjoongwon.cvi.post.dto.PostResponse;
 import com.backjoongwon.cvi.post.application.PostService;
 import java.net.URI;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -20,8 +23,9 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/users/{userId}")
-    public ResponseEntity<PostResponse> create(@PathVariable Long userId, @RequestBody PostRequest postRequest) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@PathVariable Long userId, @RequestBody PostRequest postRequest, HttpServletResponse servletResponse) {
         PostResponse postResponse = postService.create(userId, postRequest);
-        return ResponseEntity.created(URI.create("/api/v1/posts/" + postResponse.getId())).body(postResponse);
+        servletResponse.setHeader("Location", "/api/v1/posts/" + postResponse.getId());
     }
 }
--- backend/src/test/java/com/backjoongwon/cvi/post/ui/PostControllerTest.java
+++ backend/src/test/java/com/backjoongwon/cvi/post/ui/PostControllerTest.java
@@ -5,26 +5,18 @@ import com.backjoongwon.cvi.ApiDocument;
 import com.backjoongwon.cvi.post.application.PostService;
 import com.backjoongwon.cvi.post.dto.PostRequest;
 import com.backjoongwon.cvi.post.dto.PostResponse;
-import com.backjoongwon.cvi.post.ui.PostController;
-import com.backjoongwon.cvi.user.application.UserService;
-import com.backjoongwon.cvi.user.dto.UserRequest;
 import com.backjoongwon.cvi.user.dto.UserResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
-import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -36,38 +28,37 @@ class PostControllerTest extends ApiDocument {
     private static final Long POST_ID = 100L;
 
     private PostRequest request;
-    private PostResponse response;
+    private PostResponse expectedResponse;
     @MockBean
     private PostService postService;
 
     @BeforeEach
     void init() {
         request = new PostRequest("글 내용", "화이자");
-        response = new PostResponse(POST_ID);
+        expectedResponse = new PostResponse(POST_ID);
     }
 
     @DisplayName("글 등록 - 성공")
     @Test
     void createPost() throws Exception {
         //given
-        given(postService.create(USER_ID, any(PostRequest.class))).willReturn(response);
+        given(postService.create(any(Long.class), any(PostRequest.class))).willReturn(expectedResponse);
         //when
-        ResultActions response = 글_등록(USER_ID, request);
+        ResultActions response = 글_등록_요청(USER_ID, request);
         //then
-        // 사용자_회원가입_성공함(response, userResponse);
+        글_등록_성공함(response, expectedResponse);
     }
 
-    private ResultActions 글_등록(Long userId, PostRequest request) throws Exception {
-        return mockMvc.perform(post("/api/v1/posts/users/{u")
+    private ResultActions 글_등록_요청(Long userId, PostRequest request) throws Exception {
+        return mockMvc.perform(post("/api/v1/posts/users/" + userId)
             .contentType(MediaType.APPLICATION_JSON)
             .content(toJson(request)));
     }
 
-    private void 사용자_회원가입_성공함(ResultActions response, UserResponse userResponse) throws Exception {
+    private void 글_등록_성공함(ResultActions response, PostResponse expectedResponse) throws Exception {
         response.andExpect(status().isCreated())
-                .andExpect(header().string("Location", "/api/v1/users/" + 1))
-                .andExpect(content().json(toJson(userResponse)))
+                .andExpect(header().string("Location", "/api/v1/posts/" + expectedResponse.getId()))
                 .andDo(print())
-                .andDo(toDocument("user-signup"));
+                .andDo(toDocument("post-create"));
     }
 }
