52beac81db30f1f5513f08d95c22238c70872a0b
jum0 <wnsah052@naver.com>
2021-07-11 15:24:04 +0900

    feat: Frame 컴포넌트, 스토리북 구현

---
+++ frontend/src/components/Frame/Frame.styles.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1.6rem;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+
+  ${({ styles }) => styles};
+`;
+
+export { Container };
