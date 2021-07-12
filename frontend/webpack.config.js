231e5ee73d33ef8f34d2f03ab78ce6fa5847cb2a
jum0 <wnsah052@naver.com>
2021-07-12 18:41:23 +0900

    chore: svg 관련 초기 설정 추가
    Co-authored-by: HyuuunjuKim <HyuuunjuKim@users.noreply.github.com>

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -31,8 +31,12 @@ const config = {
         },
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
       },
     ],
   },
