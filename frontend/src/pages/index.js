e7874a64328041d3c4cf224fe13db2be2f02cf5e
jum0 <wnsah052@naver.com>
2021-07-15 00:13:49 +0900

    feat: LoginPage, SignupPage 컴포넌트 라우팅 적용
    Co-authored-by: HyuuunjuKim <HyuuunjuKim@users.noreply.github.com>

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,6 @@
 import HomePage from './HomePage/HomePage';
 import ReviewPage from './ReviewPage/ReviewPage';
+import LoginPage from './LoginPage/LoginPage';
+import SignupPage from './SignupPage/SignupPage';
 
-export { HomePage, ReviewPage };
+export { HomePage, ReviewPage, LoginPage, SignupPage };
