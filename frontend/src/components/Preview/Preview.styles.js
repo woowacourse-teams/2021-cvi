b091e4f7239122d10385968096b5f651e7ad548b
jum0 <wnsah052@naver.com>
2021-07-14 18:06:27 +0900

    feat: Preview 컴포넌트 구현
    Co-authored-by: HyuuunjuKim <HyuuunjuKim@gus7wn@gmail.com>

---
+++ frontend/src/components/Preview/Preview.styles.js
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+const Title = styled.h3`
+  margin-bottom: 1rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2rem 1.5rem;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export { Title, Container, ButtonContainer };
