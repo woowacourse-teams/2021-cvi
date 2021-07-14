d96493652118039e47848837bca6b8ad99718920
jum0 <wnsah052@naver.com>
2021-07-15 00:11:18 +0900

    feat: SideBar 컴포넌트 activeStyles 적용
    Co-authored-by: HyuuunjuKim <HyuuunjuKim@users.noreply.github.com>

--- frontend/src/components/SideBar/SideBar.js
+++ frontend/src/components/SideBar/SideBar.js
@@ -1,9 +1,14 @@
-import React from 'react';
 import Frame from '../Frame/Frame';
 import HomeIcon from '../../assets/icons/home.svg';
 import ReviewIcon from '../../assets/icons/review.svg';
 import LoginIcon from '../../assets/icons/login.svg';
-import { Container, LogoContainer, MenuContainer, LinkContainer } from './SideBar.styles';
+import {
+  Container,
+  LogoContainer,
+  MenuContainer,
+  LinkContainer,
+  selectedNavStyles,
+} from './SideBar.styles';
 import { FONT_COLOR, PATH } from '../../constants';
 
 const SideBar = () => {
@@ -15,14 +20,14 @@ const SideBar = () => {
           <HomeIcon width="80" height="80" stroke={FONT_COLOR.PURPLE_GRAY} />
         </LogoContainer>
         <MenuContainer>
-          <LinkContainer to={PATH.HOME}>
+          <LinkContainer exact to={PATH.HOME} activeStyle={selectedNavStyles}>
             <HomeIcon width="20" height="20" stroke={FONT_COLOR.PURPLE_GRAY} /> 홈
           </LinkContainer>
-          <LinkContainer to={PATH.REVIEW}>
+          <LinkContainer to={PATH.REVIEW} activeStyle={selectedNavStyles}>
             <ReviewIcon width="20" height="20" stroke={FONT_COLOR.PURPLE_GRAY} /> 접종후기
           </LinkContainer>
         </MenuContainer>
-        <LinkContainer>
+        <LinkContainer to={PATH.LOGIN} activeStyle={selectedNavStyles}>
           <LoginIcon width="24" height="24" stroke={FONT_COLOR.PURPLE_GRAY} /> 로그인
         </LinkContainer>
       </Container>
