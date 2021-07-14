a92570b0954d4f6e78ccae93e2bb052e04c35366
Ubuntu <ubuntu@ip-192-168-1-23.ap-northeast-2.compute.internal>
2021-07-14 12:39:34 +0000

    fix: 빌드 스크립트 프로세스 종료 로직 수정

--- backend/deploy/deploy.sh
+++ backend/deploy/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #!/bin/bash in the top of your scripts then you are telling your system to use bash as a default shell.
 
-REPOSITORY=/home/ubuntu/ # 경로 설정
-PROJECT_NAME=2021-cvi/backend
+REPOSITORY=/home/ubuntu/2021-cvi/ # 경로 설정
+PROJECT_NAME=backend
+PROCESS_NAME=cvi
 
 cd $REPOSITORY/$PROJECT_NAME/ # 경로로 접근
 
@@ -24,7 +25,7 @@ JAR_LOCATION=$(find ./* -name "*jar" | grep cvi-0.0.1-SNAPSHOT.jar)
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}[-A-z0-9.]*.jar$) # 실행시켜져있>는 jar pid 받기
+CURRENT_PID=$(pgrep -f ${PROCESS_NAME}[-A-z0-9.]*.jar$) # 실행시켜져있>는 jar pid 받기
 
 
 if [ -z "$CURRENT_PID" ]; then # -z 플래그는 null인것을 체크함, PID가 null인 경우 if절 안으로 들어감
