5d58a5714ad7934349dc85a47f801b27e3befdb1
Ubuntu <ubuntu@ip-192-168-1-23.ap-northeast-2.compute.internal>
2021-07-14 13:37:16 +0000

    fix: 배포 스크립트 수정

--- backend/deploy/deploy.sh
+++ backend/deploy/deploy.sh
@@ -3,17 +3,13 @@
 
 REPOSITORY=/home/ubuntu/2021-cvi/ # 경로 설정
 PROJECT_NAME=backend
-PROCESS_NAME=cvi
 
 cd $REPOSITORY/$PROJECT_NAME/ # 경로로 접근
 
-echo "> git reset --hard origin/HEAD"
-
- git reset --hard origin/HEAD # 깃허브 초기화
-
-echo "> git pull origin develop"
+echo "> git fetch origin deploy, git reset --hard FETCH_HEAD"
 
 git pull origin develop # pull 땡겨오기
+git reset --hard FETCH_HEAD
 
 echo "> 프로젝트 Build 시작"
 
@@ -25,8 +21,7 @@ JAR_LOCATION=$(find ./* -name "*jar" | grep cvi-0.0.1-SNAPSHOT.jar)
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f ${PROCESS_NAME}[-A-z0-9.]*.jar$) # 실행시켜져있>는 jar pid 받기
-
+CURRENT_PID=$(pgrep -fl ./build/libs/cvi-0.0.1-SNAPSHOT.jar | grep java | awk '{print $1}')
 
 if [ -z "$CURRENT_PID" ]; then # -z 플래그는 null인것을 체크함, PID가 null인 경우 if절 안으로 들어감
     echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
