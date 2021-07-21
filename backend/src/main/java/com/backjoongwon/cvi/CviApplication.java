0e2c1e629b7a22cb8539fd21fad1c61b8d911e46
youngbin <thisisyoungbin@gmail.com>
2021-07-21 20:01:10 +0900

    feat: @PostConstruct를 활용한 서버 시간 설정

--- backend/src/main/java/com/backjoongwon/cvi/CviApplication.java
+++ backend/src/main/java/com/backjoongwon/cvi/CviApplication.java
@@ -3,8 +3,16 @@ package com.backjoongwon.cvi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CviApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CviApplication.class, args);
