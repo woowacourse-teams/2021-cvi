6d332eaf3eb63b8f1496b092a4b0176c9aa2ffc6
HyuuunjuKim <gus7wn@gmail.com>
2021-07-22 17:38:26 +0900

    refactor: 컴포넌트 return 방식 통일
    Co-authored-by: jum0 wnsah052@naver.com

--- frontend/src/components/Selection/Selection.js
+++ frontend/src/components/Selection/Selection.js
@@ -5,26 +5,24 @@ import Button from '../Button/Button';
 import { BUTTON_BACKGROUND_TYPE, BUTTON_SIZE_TYPE } from '../Button/Button.styles';
 import { PALETTE } from '../../constants';
 
-const Selection = ({ selectionList, selectedItem, setSelectedItem }) => {
-  return (
-    <Container>
-      {selectionList.map((selectionItem) => (
-        <Button
-          key={selectionItem}
-          backgroundType={BUTTON_BACKGROUND_TYPE.TEXT}
-          sizeType={BUTTON_SIZE_TYPE.LARGE}
-          color={PALETTE.NAVY300}
-          isSelected={selectedItem === selectionItem}
-          selectedStyles={buttonSelectedStyles[selectionItem] || defaultButtonStyles}
-          type="button"
-          onClick={() => setSelectedItem(selectionItem)}
-        >
-          {selectionItem}
-        </Button>
-      ))}
-    </Container>
-  );
-};
+const Selection = ({ selectionList, selectedItem, setSelectedItem }) => (
+  <Container>
+    {selectionList.map((selectionItem) => (
+      <Button
+        key={selectionItem}
+        backgroundType={BUTTON_BACKGROUND_TYPE.TEXT}
+        sizeType={BUTTON_SIZE_TYPE.LARGE}
+        color={PALETTE.NAVY300}
+        isSelected={selectedItem === selectionItem}
+        selectedStyles={buttonSelectedStyles[selectionItem] || defaultButtonStyles}
+        type="button"
+        onClick={() => setSelectedItem(selectionItem)}
+      >
+        {selectionItem}
+      </Button>
+    ))}
+  </Container>
+);
 
 Selection.propTypes = {
   selectionList: PropTypes.array.isRequired,
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -2,13 +2,11 @@ import { Container } from './HomePage.styles';
 import VaccinationState from '../../components/VaccinationState/VaccinationState';
 import Preview from '../../components/Preview/Preview';
 
-const HomePage = () => {
-  return (
-    <Container>
-      <VaccinationState title="접종 현황" />
-      <Preview title="접종 후기" />
-    </Container>
-  );
-};
+const HomePage = () => (
+  <Container>
+    <VaccinationState title="접종 현황" />
+    <Preview title="접종 후기" />
+  </Container>
+);
 
 export default HomePage;
