f971bb333c2372414f1309df3c6300ab2ac753ef
youngbin <thisisyoungbin@gmail.com>
2021-07-25 00:44:49 +0900

    test: Authorization Manager 테스트 케이스 추가(카카오)

--- backend/src/main/java/com/backjoongwon/cvi/auth/domain/authorization/AuthorizationManager.java
+++ backend/src/main/java/com/backjoongwon/cvi/auth/domain/authorization/AuthorizationManager.java
@@ -2,14 +2,14 @@ package com.backjoongwon.cvi.auth.domain.authorization;
 
 import com.backjoongwon.cvi.auth.domain.profile.UserInformation;
 import com.backjoongwon.cvi.common.exception.InvalidOperationException;
-import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Objects;
 
 @Component
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@RequiredArgsConstructor
 public class AuthorizationManager {
 
     private final Map<String, Authorization> authorizationMap;
@@ -20,15 +20,18 @@ public class AuthorizationManager {
     }
 
     private Authorization extractAuthorization(SocialProvider provider) {
-        if (provider == null) {
-            throw new InvalidOperationException("해당 OAuth 제공자가 존재하지 않습니다");
-        }
-
+        validateSocialProvider(provider);
         String key = provider.convertToComponentName();
         if (authorizationMap.containsKey(key)) {
             return authorizationMap.get(key);
         }
         throw new InvalidOperationException("해당 OAuth 제공자가 존재하지 않습니다");
+    }
+
+    private void validateSocialProvider(SocialProvider provider) {
+        if (Objects.isNull(provider)) {
+            throw new InvalidOperationException("해당 OAuth 제공자가 존재하지 않습니다");
+        }
     }
 }
 
--- backend/src/test/java/com/backjoongwon/cvi/auth/domain/authorization/AuthorizationManagerTest.java
+++ backend/src/test/java/com/backjoongwon/cvi/auth/domain/authorization/AuthorizationManagerTest.java
@@ -1,5 +1,6 @@
 package com.backjoongwon.cvi.auth.domain.authorization;
 
+import com.backjoongwon.cvi.auth.domain.profile.KakaoProfile;
 import com.backjoongwon.cvi.auth.domain.profile.NaverProfile;
 import com.backjoongwon.cvi.auth.domain.profile.UserInformation;
 import com.backjoongwon.cvi.common.exception.InvalidOperationException;
@@ -23,6 +24,9 @@ import static org.mockito.BDDMockito.willReturn;
 class AuthorizationManagerTest {
 
     private static final String NAVER_PROFILE_RESPONSE = "{\"resultcode\":\"00\",\"message\":\"success\",\"response\":{\"id\":\"NAVER_ID\",\"nickname\":\"yon\",\"profile_image\":\"naver.com/profile\"}}";
+    private static final String KAKAO_PROFILE_RESPONSE = "{\"id\":1816688137,\"connected_at\":\"2021-07-22T05:43:16Z\",\"properties\":{\"nickname\":\"김영빈\"},\"kakao_account\":{\"profile_nickname_needs_agreement\":false,\"profile_image_needs_agreement\":false,\"profile\":{\"nickname\":\"김영빈\",\"thumbnail_image_url\":\"http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_110x110.jpg\",\"profile_image_url\":\"http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_640x640.jpg\",\"is_default_image\":true}}}";
+    public static final String STATE = "STATE";
+    public static final String SOCIAL_CODE = "CODE";
 
     @Autowired
     private AuthorizationManager authorizationManager;
@@ -34,31 +38,44 @@ class AuthorizationManagerTest {
     private KakaoAuthorization kakaoAuthorization;
 
     private ObjectMapper objectMapper = new ObjectMapper();
-    private UserInformation userInfo;
+    private UserInformation naverUserInfo;
+    private UserInformation kakaoUserInfo;
 
     @BeforeEach
     void setUp() throws JsonProcessingException {
         NaverProfile naverProfile = objectMapper.readValue(NAVER_PROFILE_RESPONSE, NaverProfile.class);
-        userInfo = UserInformation.of(naverProfile);
+        naverUserInfo = UserInformation.of(naverProfile);
+        KakaoProfile kakaoProfile = objectMapper.readValue(KAKAO_PROFILE_RESPONSE, KakaoProfile.class);
+        kakaoUserInfo = UserInformation.of(kakaoProfile);
     }
 
     @DisplayName("Naver Authorization 매니저 유저 정보 요청 - 성공")
     @Test
-    void requestUserInfo() {
+    void requestNaverUserInfo() {
         //given
-        willReturn(userInfo).given(naverAuthorization).requestProfile("aa", "aa");
+        willReturn(naverUserInfo).given(naverAuthorization).requestProfile(SOCIAL_CODE, STATE);
         //when
         //then
-        assertThat(authorizationManager.requestUserInfo(SocialProvider.NAVER, "aa", "aa")).isEqualTo(userInfo);
+        assertThat(authorizationManager.requestUserInfo(SocialProvider.NAVER, SOCIAL_CODE, STATE)).isEqualTo(naverUserInfo);
     }
 
-    @DisplayName("Authorization 매니저 유저 정보 요청 - 실패 - Provider가 유효하지 않은 경우")
+    @DisplayName("Kakao Authorization 매니저 유저 정보 요청 - 성공")
     @Test
-    void requestUserInfoFailureWhenInvalidSocialProvider() {
+    void requestKakaoUserInfo() {
+        //given
+        willReturn(kakaoUserInfo).given(kakaoAuthorization).requestProfile(SOCIAL_CODE, null);
+        //when
+        //then
+        assertThat(authorizationManager.requestUserInfo(SocialProvider.KAKAO, SOCIAL_CODE, null)).isEqualTo(kakaoUserInfo);
+    }
+
+    @DisplayName("Authorization 매니저 유저 정보 요청 - 실패 - Provider가 유효하지 않은 경우 - Naver")
+    @Test
+    void requestUserInfoFailureWhenInvalidSocialProviderNaver() {
         //given
         //when
         //then
-        assertThatThrownBy(() -> authorizationManager.requestUserInfo(null, "aa", "aa"))
+        assertThatThrownBy(() -> authorizationManager.requestUserInfo(null, SOCIAL_CODE, STATE))
                 .isExactlyInstanceOf(InvalidOperationException.class)
                 .hasMessage("해당 OAuth 제공자가 존재하지 않습니다");
     }
