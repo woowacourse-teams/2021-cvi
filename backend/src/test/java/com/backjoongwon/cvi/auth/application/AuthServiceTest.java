956b512628ec0a7ee91c284ef9d0d8cf80447472
youngbin <thisisyoungbin@gmail.com>
2021-07-24 18:14:07 +0900

    refactor: AuthServiceTest 실패하는 경우 추가

--- backend/src/test/java/com/backjoongwon/cvi/auth/application/AuthServiceTest.java
+++ backend/src/test/java/com/backjoongwon/cvi/auth/application/AuthServiceTest.java
@@ -6,6 +6,7 @@ import com.backjoongwon.cvi.auth.domain.authorization.SocialProvider;
 import com.backjoongwon.cvi.auth.domain.profile.NaverProfile;
 import com.backjoongwon.cvi.auth.domain.profile.UserInformation;
 import com.backjoongwon.cvi.auth.dto.AuthRequest;
+import com.backjoongwon.cvi.common.exception.MappingFailureException;
 import com.backjoongwon.cvi.user.domain.AgeRange;
 import com.backjoongwon.cvi.user.domain.JwtTokenProvider;
 import com.backjoongwon.cvi.user.domain.User;
@@ -23,6 +24,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @DisplayName("소셜로그인 비즈니스 흐름 테스트")
@@ -65,15 +67,16 @@ class AuthServiceTest {
                 .socialProvider(SocialProvider.NAVER)
                 .build();
         token = "naver_auth_token";
-        NaverProfile naverProfile = objectMapper.readValue(NAVER_PROFILE_RESPONSE, NaverProfile.class);;
+
+        NaverProfile naverProfile = objectMapper.readValue(NAVER_PROFILE_RESPONSE, NaverProfile.class);
         userInfo = UserInformation.of(naverProfile);
 
         userResponse = UserResponse.of(user, token);
     }
 
-    @DisplayName("네이버 소셜 로그인 - 성공")
+    @DisplayName("네이버 소셜 로그인 - 성공 - 이미 가입한 유저인 경우")
     @Test
-    void authenticate() {
+    void authenticateWhenUserIsExists() {
         //given
         when(userRepository.findBySocialProviderAndSocialId(SocialProvider.NAVER, "NAVER_ID")).thenReturn(Optional.of(user));
         when(jwtTokenProvider.createToken(user.getId())).thenReturn(token);
@@ -82,5 +85,47 @@ class AuthServiceTest {
         UserResponse expected = authService.authenticate(authRequest);
         //then
         assertThat(expected.getNickname()).isEqualTo(userResponse.getNickname());
+    }
+
+    @DisplayName("네이버 소셜 로그인 - 성공 - 신규 유저인 경우")
+    @Test
+    void authenticateWhenUserIsNotExists() {
+        //given
+        when(userRepository.findBySocialProviderAndSocialId(SocialProvider.NAVER, "NEW_ID")).thenReturn(Optional.of(user));
+        when(jwtTokenProvider.createToken(user.getId())).thenReturn(token);
+        when(authorizationManager.requestUserInfo(authRequest.getProvider(), authRequest.getCode(), authRequest.getState())).thenReturn(userInfo);
+        //when
+        UserResponse expected = authService.authenticate(authRequest);
+        //then
+        assertThat(expected.getNickname()).isNull();
+        assertThat(expected.getId()).isNull();
+    }
+
+    @DisplayName("네이버 소셜 로그인 - 실패 - 잘못된 CODE인 경우")
+    @Test
+    void authenticateFailureWhenNotValidCode() {
+        //given
+        AuthRequest invalidRequest = new AuthRequest(SocialProvider.NAVER, "INVALID_SOCIAL_CODE", "STATE");
+        when(userRepository.findBySocialProviderAndSocialId(SocialProvider.NAVER, "NAVER_ID")).thenReturn(Optional.of(user));
+        when(jwtTokenProvider.createToken(user.getId())).thenReturn(token);
+        when(authorizationManager.requestUserInfo(invalidRequest.getProvider(), invalidRequest.getCode(), invalidRequest.getState())).thenThrow(MappingFailureException.class);
+        //when
+        //then
+        assertThatThrownBy(() -> authService.authenticate(invalidRequest))
+                .isExactlyInstanceOf(MappingFailureException.class);
+    }
+
+    @DisplayName("네이버 소셜 로그인 - 실패 - 잘못된 STATE인 경우")
+    @Test
+    void authenticateFailureWhenNotValidState() {
+        //given
+        AuthRequest invalidRequest = new AuthRequest(SocialProvider.NAVER, "SOCIAL_CODE", "INVALID_STATE");
+        when(userRepository.findBySocialProviderAndSocialId(SocialProvider.NAVER, "NAVER_ID")).thenReturn(Optional.of(user));
+        when(jwtTokenProvider.createToken(user.getId())).thenReturn(token);
+        when(authorizationManager.requestUserInfo(invalidRequest.getProvider(), invalidRequest.getCode(), invalidRequest.getState())).thenThrow(MappingFailureException.class);
+        //when
+        //then
+        assertThatThrownBy(() -> authService.authenticate(invalidRequest))
+                .isExactlyInstanceOf(MappingFailureException.class);
     }
 }
