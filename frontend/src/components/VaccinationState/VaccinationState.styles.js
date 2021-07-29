295fd1dfd4c31bf72f939d453b32c9ac39c00668
jum0 <wnsah052@naver.com>
2021-07-29 12:02:13 +0900

    feat: VaccinationState 컴포넌트 구현
    Co-authored-by: HyuuunjuKim <HyuuunjuKim@users.noreply.github.com>

--- frontend/src/components/VaccinationState/VaccinationState.styles.js
+++ frontend/src/components/VaccinationState/VaccinationState.styles.js
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { FONT_COLOR } from '../../constants';
 
 const Container = styled.div`
   margin-top: 0.3rem;
@@ -8,4 +9,30 @@ const Title = styled.h3`
   margin-bottom: 1rem;
 `;
 
-export { Container, Title };
+const FrameContent = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(3, 1fr);
+  margin: 2.4rem 2.7rem;
+`;
+
+const PrimaryState = styled.div`
+  display: flex;
+  margin-right: auto;
+`;
+
+const Info = styled.div`
+  color: ${FONT_COLOR.GRAY};
+  margin: 1.2rem 0 1.2rem 2.4rem;
+  font-size: 1.4rem;
+  font-weight: 500;
+`;
+
+const InfoTitle = styled.div`
+  color: ${FONT_COLOR.BLACK};
+  font-weight: 600;
+  margin-bottom: 1rem;
+  font-size: 1.6rem;
+`;
+
+export { Container, Title, FrameContent, PrimaryState, Info, InfoTitle };
