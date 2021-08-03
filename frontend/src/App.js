057dfb7dc56e59a81ca591606354a44f4bd4ea46
HyuuunjuKim <gus7wn@gmail.com>
2021-08-03 18:54:35 +0900

    chore: notistack 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import { SnackbarProvider } from 'notistack';
 import GlobalStyles from './GlobalStyles';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { PATH } from './constants';
@@ -20,58 +19,50 @@ import { BaseLayout } from './components/common';
 const App = () => {
   return (
     <>
-      <SnackbarProvider
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        maxSnack={3}
-      >
-        <GlobalStyles />
-        <BrowserRouter>
-          <BaseLayout>
-            <Switch>
-              <Route exact path={PATH.HOME} component={HomePage} />
-              <Route exact path={PATH.REVIEW} component={ReviewPage} />
-              <Route exact path={`${PATH.REVIEW}/:id`} component={ReviewDetailPage} />
-              <Route exact path={`${PATH.REVIEW}/:id/edit`} component={ReviewEditPage} />
-              <Route exact path={PATH.LOGIN} component={LoginPage} />
-              <Route exact path={PATH.SIGNUP} component={SignupPage} />
-              <Route exact path={PATH.MY_PAGE} component={MyPage} />
-              <Route
-                exact
-                path={PATH.MY_PAGE_ACCOUNT}
-                render={() => (
-                  <MyPage>
-                    <MyPageAccount />
-                  </MyPage>
-                )}
-              />
-              <Route
-                exact
-                path={PATH.MY_PAGE_SHOT_VERIFICATION}
-                render={() => (
-                  <MyPage>
-                    <MyPageShotVerification />
-                  </MyPage>
-                )}
-              />
-              <Route
-                exact
-                path={PATH.MY_PAGE_REVIEWS}
-                render={() => (
-                  <MyPage>
-                    <MyPageReviews />
-                  </MyPage>
-                )}
-              />
-              <Route exact path={PATH.OAUTH_KAKAO} component={OAuthPage} />
-              <Route exact path={PATH.OAUTH_NAVER} component={OAuthPage} />
-              <Redirect to={PATH.HOME} />
-            </Switch>
-          </BaseLayout>
-        </BrowserRouter>
-      </SnackbarProvider>
+      <GlobalStyles />
+      <BrowserRouter>
+        <BaseLayout>
+          <Switch>
+            <Route exact path={PATH.HOME} component={HomePage} />
+            <Route exact path={PATH.REVIEW} component={ReviewPage} />
+            <Route exact path={`${PATH.REVIEW}/:id`} component={ReviewDetailPage} />
+            <Route exact path={`${PATH.REVIEW}/:id/edit`} component={ReviewEditPage} />
+            <Route exact path={PATH.LOGIN} component={LoginPage} />
+            <Route exact path={PATH.SIGNUP} component={SignupPage} />
+            <Route exact path={PATH.MY_PAGE} component={MyPage} />
+            <Route
+              exact
+              path={PATH.MY_PAGE_ACCOUNT}
+              render={() => (
+                <MyPage>
+                  <MyPageAccount />
+                </MyPage>
+              )}
+            />
+            <Route
+              exact
+              path={PATH.MY_PAGE_SHOT_VERIFICATION}
+              render={() => (
+                <MyPage>
+                  <MyPageShotVerification />
+                </MyPage>
+              )}
+            />
+            <Route
+              exact
+              path={PATH.MY_PAGE_REVIEWS}
+              render={() => (
+                <MyPage>
+                  <MyPageReviews />
+                </MyPage>
+              )}
+            />
+            <Route exact path={PATH.OAUTH_KAKAO} component={OAuthPage} />
+            <Route exact path={PATH.OAUTH_NAVER} component={OAuthPage} />
+            <Redirect to={PATH.HOME} />
+          </Switch>
+        </BaseLayout>
+      </BrowserRouter>
     </>
   );
 };
--- frontend/src/pages/ReviewDetailPage/ReviewDetailPage.js
+++ frontend/src/pages/ReviewDetailPage/ReviewDetailPage.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useSnackbar } from 'notistack';
 import {
   Container,
   FrameContent,
