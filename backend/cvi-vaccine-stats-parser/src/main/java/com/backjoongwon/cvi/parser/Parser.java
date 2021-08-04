cc7da033a41b1f94080479bcc54b5cb18f069d04
TaeJeong Kim <xowjd41@naver.com>
2021-08-04 11:16:07 +0900

    feat: URLWapper 생성 및 테스트 코드 작성

--- backend/cvi-vaccine-stats-parser/src/main/java/com/backjoongwon/cvi/parser/Parser.java
+++ backend/cvi-vaccine-stats-parser/src/main/java/com/backjoongwon/cvi/parser/Parser.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class Parser {
 
@@ -15,7 +14,7 @@ public class Parser {
 
     public String parse(String url) {
         try {
-            HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+            HttpURLConnection con = new URLWrapper(url).openConnection();
             con.setConnectTimeout(5000);
             con.setReadTimeout(5000);
             con.setRequestMethod(GET_METHOD);
