6e9a2ddeb6fc5616067dd9e86abab0b8fbce75e9
TaeJeong Kim <xowjd41@naver.com>
2021-08-05 21:50:33 +0900

    refactor: 서버 시작시 데이터 요청 로직 구현

--- backend/src/main/java/com/backjoongwon/cvi/publicdata/application/PublicDataScheduler.java
+++ backend/src/main/java/com/backjoongwon/cvi/publicdata/application/PublicDataScheduler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 
 @Slf4j
@@ -14,10 +15,17 @@ public class PublicDataScheduler {
 
     private final PublicDataService publicDataService;
 
+    @PostConstruct
+    private void initializeVaccinationDate() {
+        log.info("서버시작, 백신접종률 api요청 시작:");
+        publicDataService.saveVaccinationStatistics(LocalDateTime.now());
+        log.info("서버시작, 백신접종률 api요청 완료 및 데이터베이스 저장 완료");
+    }
+
     @Scheduled(cron = "0 0 10 * * ?")
     private void scheduleVaccinationData() {
-        log.info("공공데이터 api요청 시작:");
+        log.info("백신접종률 api요청 시작:");
         publicDataService.saveVaccinationStatistics(LocalDateTime.now());
-        log.info("공공데이터 api요청 완료 및 데이터베이스 저장 완료");
+        log.info("백신접종률 api요청 완료 및 데이터베이스 저장 완료");
     }
 }
