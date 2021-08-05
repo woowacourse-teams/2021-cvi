b95a95d2e52416caf7a3e25fe4a44cb39abf29b9
younghoonkwon <younghoon.kwon91@gmail.com>
2021-08-05 16:00:41 +0900

    feat: defaultValue 추가

--- backend/src/main/java/com/backjoongwon/cvi/post/ui/PostController.java
+++ backend/src/main/java/com/backjoongwon/cvi/post/ui/PostController.java
@@ -48,7 +48,8 @@ public class PostController {
     @GetMapping("/paging")
     @ResponseStatus(HttpStatus.OK)
     public List<PostResponse> findByVaccineTypeAndPaging(@RequestParam(defaultValue = "ALL") VaccinationType vaccinationType,
-                                                         @RequestParam Long lastPostId, @RequestParam int size,
+                                                         @RequestParam(defaultValue =  "#{T(java.lang.String).valueOf(T(java.lang.Long).MAX_VALUE)}") Long lastPostId,
+                                                         @RequestParam(defaultValue =  "6") int size,
                                                          @RequestParam(defaultValue = "CREATED_AT_DESC") Sort sort,
                                                          @RequestParam(defaultValue = "#{T(java.lang.String).valueOf(T(java.lang.Integer).MAX_VALUE)}") int fromHoursBefore,
                                                          @AuthenticationPrincipal Optional<User> user) {
