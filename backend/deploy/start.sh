a138d8654b121d84e6955802c09648ddafb1ed97
younghoonkwon <younghoon.kwon91@gmail.com>
2021-08-09 00:38:01 +0900

    refactor: start.sh 수정

--- backend/deploy/start.sh
+++ backend/deploy/start.sh
@@ -28,7 +28,7 @@ if [[ "$1" == prod ]]
 then
   nohup java -jar \
     -Duser.timezone=Asia/Seoul \
-    -Dspring.config.location=classpath:/application.yml,/home/ubuntu/deploy/application-db.yml,/home/ubuntu/deploy/application-jwt.yml \
+    -Dspring.config.location=classpath:/application.yml,/home/ubuntu/deploy/application-db.yml,/home/ubuntu/deploy/application-jwt.yml, /home/ubuntu/deploy/application-client-secret.yml\
     -Dspring.profiles.active=$IDLE_PROFILE,"$1"\
     $JAR_LOCATION > ~/nohup.out 2>&1 &
 else
