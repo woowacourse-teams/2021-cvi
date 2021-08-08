75999a0ea12f62e79beeead4e5f352b410715f3e
HyuuunjuKim <gus7wn@gmail.com>
2021-08-08 17:55:49 +0900

    feat: VaccinationState에 더보기 버튼 추가

--- frontend/src/components/VaccinationState/VaccinationState.js
+++ frontend/src/components/VaccinationState/VaccinationState.js
@@ -1,6 +1,9 @@
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { css } from '@emotion/react';
 import {
   Container,
+  TopContainer,
   Title,
   FrameContent,
   PrimaryState,
@@ -8,18 +11,43 @@ import {
   InfoTitle,
   IncreaseIcon,
 } from './VaccinationState.styles';
-import { Frame, DonutChart } from '../common';
+import { Frame, DonutChart, Button } from '../common';
 import { requestVaccinationStateList } from '../../requests';
 import { useFetch } from '../../hooks';
 import { numberWithCommas } from '../../utils';
+import { BUTTON_BACKGROUND_TYPE } from '../common/Button/Button.styles';
+import { FONT_COLOR, PATH } from '../../constants';
+import { RightArrowIcon } from '../../assets/icons';
 
-const VaccinationState = ({ title, withWorld }) => {
+const VaccinationState = ({ title, withWorld, withViewMore }) => {
+  const history = useHistory();
+
   const { response, error, loading } = useFetch([], requestVaccinationStateList);
   const koreanState = response[0];
 
+  const goStatePage = () => {
+    history.push(PATH.STATE);
+  };
+
   return (
     <Container>
-      {title && <Title>{title}</Title>}
+      <TopContainer>
+        {title && <Title>{title}</Title>}
+        {withViewMore && (
+          <Button
+            backgroundType={BUTTON_BACKGROUND_TYPE.TEXT}
+            color={FONT_COLOR.BLUE_GRAY}
+            withIcon={true}
+            styles={css`
+              padding-right: 0;
+            `}
+            onClick={goStatePage}
+          >
+            <div>더보기</div>
+            <RightArrowIcon width="18" height="18" stroke={FONT_COLOR.BLUE_GRAY} />
+          </Button>
+        )}
+      </TopContainer>
       <Frame width="100%" showShadow={true}>
         <FrameContent withWorld={withWorld}>
           <PrimaryState>
@@ -62,11 +90,13 @@ const VaccinationState = ({ title, withWorld }) => {
 VaccinationState.propTypes = {
   title: PropTypes.string,
   withWorld: PropTypes.bool,
+  withViewMore: PropTypes.bool,
 };
 
 VaccinationState.defaultProps = {
   title: '',
   withWorld: true,
+  withViewMore: false,
 };
 
 export default VaccinationState;
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -4,7 +4,7 @@ import { Container } from './HomePage.styles';
 
 const HomePage = () => (
   <Container>
-    <VaccinationState title="접종 현황" />
+    <VaccinationState title="접종 현황" withViewMore={true} />
     <Preview title="최신 글" reviewType={FILTER_TYPE.CREATED_AT} />
     {/* <Preview title="좋아요 많은 글" reviewType={FILTER_TYPE.LIKE_COUNT} /> */}
     <Preview title="조회수 높은 글" reviewType={FILTER_TYPE.VIEW_COUNT} />
