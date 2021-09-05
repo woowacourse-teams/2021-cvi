d05529f897c50caa0d04a7d07814eed83364e27e
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-06 02:29:23 +0900

    refactor: DataLoader dev profile 에서만 동작하도록 수정

--- backend/src/main/java/com/backjoongwon/cvi/DataLoader.java
+++ backend/src/main/java/com/backjoongwon/cvi/DataLoader.java
@@ -25,7 +25,7 @@ import java.util.*;
 
 @Component
 @RequiredArgsConstructor
-@Profile("local&!test")
+@Profile("dev")
 public class DataLoader implements CommandLineRunner {
 
     private static final int USER_COUNT = 50;
