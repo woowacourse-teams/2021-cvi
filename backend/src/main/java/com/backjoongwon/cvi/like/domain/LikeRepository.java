8dd39ad483974b38bd9d061d869187a4bef85f5e
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-06 02:31:05 +0900

    refactor: 사용하지 않는 함수 삭제

--- backend/src/main/java/com/backjoongwon/cvi/like/domain/LikeRepository.java
+++ backend/src/main/java/com/backjoongwon/cvi/like/domain/LikeRepository.java
@@ -3,4 +3,5 @@ package com.backjoongwon.cvi.like.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long>, LikeQueryDsl {
+    void deleteAllByPostId(Long postId);
 }
