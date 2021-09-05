e9629a202134590035c4dc181f404d11709b4f39
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-06 03:28:39 +0900

    refactor: 사용하지 않는 함수 삭제

--- backend/src/main/java/com/backjoongwon/cvi/comment/domain/CommentRepository.java
+++ backend/src/main/java/com/backjoongwon/cvi/comment/domain/CommentRepository.java
@@ -3,5 +3,4 @@ package com.backjoongwon.cvi.comment.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentQueryDsl {
-    void deleteAllByPostId(Long postId);
 }
--- backend/src/main/java/com/backjoongwon/cvi/like/domain/LikeRepository.java
+++ backend/src/main/java/com/backjoongwon/cvi/like/domain/LikeRepository.java
@@ -3,5 +3,4 @@ package com.backjoongwon.cvi.like.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long>, LikeQueryDsl {
-    void deleteAllByPostId(Long postId);
 }
