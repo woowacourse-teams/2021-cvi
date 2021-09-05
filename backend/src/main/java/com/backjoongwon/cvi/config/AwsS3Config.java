3782018baeeb5d43b4d9633aa429ed6b6dd3a953
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-06 08:22:12 +0900

    feat: credentials 추가

--- backend/src/main/java/com/backjoongwon/cvi/config/AwsS3Config.java
+++ backend/src/main/java/com/backjoongwon/cvi/config/AwsS3Config.java
@@ -1,5 +1,6 @@
 package com.backjoongwon.cvi.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -26,7 +27,7 @@ public class AwsS3Config {
         final BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
-                //.withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
 }
