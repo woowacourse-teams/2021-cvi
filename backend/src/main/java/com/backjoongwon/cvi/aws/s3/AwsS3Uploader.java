06624834adb72b8ca274e4c6a96ab60d99393649
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-06 11:46:47 +0900

    fix: CommandLine 방식으로 수정

--- backend/src/main/java/com/backjoongwon/cvi/aws/s3/AwsS3Uploader.java
+++ backend/src/main/java/com/backjoongwon/cvi/aws/s3/AwsS3Uploader.java
@@ -1,10 +1,5 @@
 package com.backjoongwon.cvi.aws.s3;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.backjoongwon.cvi.common.exception.FileConvertException;
 import com.backjoongwon.cvi.image.ImageType;
 import lombok.RequiredArgsConstructor;
@@ -26,23 +21,30 @@ import java.util.UUID;
 @Component
 public class AwsS3Uploader {
 
-    private final AmazonS3Client amazonS3Client;
+    //private final AmazonS3Client amazonS3Client;
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String s3Bucket;
-    @Value("${cloud.aws.cloudfront.url}")
+    @Value("${aws.s3.bucket.url}")
+    private String s3BucketUrl;
+    @Value("${aws.cloudfront.url}")
     private String cloudFrontUrl;
 
     public String upload(ImageType imageType, byte[] imageBytes) {
-        final File uploadFile = convert(imageType, imageBytes);
-        return uploadToS3(uploadFile);
+        final File file = saveLocal(imageType, imageBytes);
+        return uploadToS3(file);
     }
 
     private String uploadToS3(File uploadFile) {
-        final PutObjectRequest putObjectRequest = new PutObjectRequest(s3Bucket, uploadFile.getPath(), uploadFile).withCannedAcl(CannedAccessControlList.PublicRead);
-        amazonS3Client.putObject(putObjectRequest);
+//        final PutObjectRequest putObjectRequest = new PutObjectRequest(s3Bucket, uploadFile.getPath(), uploadFile).withCannedAcl(CannedAccessControlList.PublicRead);
+//        amazonS3Client.putObject(putObjectRequest);
+        final String commandLine = String.format("sudo aws s3 mv ~/%s %s", uploadFile.getPath(), s3BucketUrl);
+        log.debug("S3로 파일 전송 명령어 : {}", commandLine);
+        try {
+            Runtime.getRuntime().exec(commandLine);
+        } catch (IOException e) {
+            log.error("Command line 실행 실패 : {}", commandLine);
+        }
         final String url = uploadFile.getPath();
-        removeLocalSavedImageFile(uploadFile);
+        //removeLocalSavedImageFile(uploadFile);
         return cloudFrontUrl + "/" + url;
     }
 
@@ -54,7 +56,7 @@ public class AwsS3Uploader {
         log.debug("이미지 파일 삭제 실패");
     }
 
-    private File convert(ImageType imageType, byte[] imageBytes) {
+    private File saveLocal(ImageType imageType, byte[] imageBytes) {
         final String fileNameExtension = imageType.getFileNameExtension();
         try {
             final File imageFile = new File("post-image-" + LocalDateTime.now() + "-" + UUID.randomUUID() + "." + fileNameExtension);
@@ -67,14 +69,14 @@ public class AwsS3Uploader {
         }
     }
 
-    public void delete(String imageS3Path) {
-        try {
-            log.debug("S3에서 삭제할 이미지 경로: {}", imageS3Path);
-            final DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(s3Bucket, imageS3Path);
-            amazonS3Client.deleteObject(deleteObjectRequest);
-            log.debug("S3의 {} 파일 삭제 성공", imageS3Path);
-        } catch (AmazonServiceException e) {
-            log.debug("S3의 {} 파일 삭제 실패", imageS3Path);
-        }
-    }
+//    public void delete(String imageS3Path) {
+//        try {
+//            log.debug("S3에서 삭제할 이미지 경로: {}", imageS3Path);
+//            final DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(s3BucketUrl, imageS3Path);
+//            amazonS3Client.deleteObject(deleteObjectRequest);
+//            log.debug("S3의 {} 파일 삭제 성공", imageS3Path);
+//        } catch (AmazonServiceException e) {
+//            log.debug("S3의 {} 파일 삭제 실패", imageS3Path);
+//        }
+//    }
 }
