0cd741b30dbbec7b08868bd96896fd5bf050bf67
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-08 15:17:05 +0900

    refactor: SpringBootTest -> DataJpaTest 로 변경

--- backend/src/test/java/com/backjoongwon/cvi/post/domain/PostRepositoryTest.java
+++ backend/src/test/java/com/backjoongwon/cvi/post/domain/PostRepositoryTest.java
@@ -15,20 +15,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("PostRepository 테스트")
 @Transactional
-@SpringBootTest
+@DataJpaTest
 class PostRepositoryTest {
 
     @MockBean
