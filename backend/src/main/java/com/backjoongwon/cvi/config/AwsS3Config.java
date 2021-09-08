5556992b19f68b3c09dff2abb401f2df8e2943e0
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-08 15:57:28 +0900

    fix: Amazon S3 관련 Test 에러 수정

--- backend/src/main/java/com/backjoongwon/cvi/config/AwsS3Config.java
+++ backend/src/main/java/com/backjoongwon/cvi/config/AwsS3Config.java
@@ -5,9 +5,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Profile("!test")
 @Configuration
 public class AwsS3Config {
 
