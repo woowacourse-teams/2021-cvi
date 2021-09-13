285f55750f3c83e1434f642a3fbb1962eb03303b
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-13 17:46:33 +0900

    fix: flyway sql파일 develop 브랜치와 동기화, Image 테이블 추가 sql 작성
    - V20210726__Create_init.sql 삭제
    - V20210810__Column_add.sql 원삭복구
    - V20210913__Create_image.sql 추가

--- backend/src/main/resources/db/migration/V20210810__Column_add.sql
+++ backend/src/main/resources/db/migration/V20210810__Column_add.sql
@@ -1,63 +1,5 @@
-create table user
-(
-    user_id          bigint auto_increment not null,
-    created_at       timestamp        default CURRENT_TIMESTAMP,
-    last_modified_at timestamp        default CURRENT_TIMESTAMP,
-    nickname         varchar(255),
-    age_range        varchar(255),
-    shot_verified    boolean not null default false,
-    social_provider  varchar(255),
-    social_id        varchar(255),
-    profile_url      varchar(255),
-    primary key (user_id),
-    unique (nickname)
-);
-
-create table post
-(
-    post_id          bigint auto_increment not null,
-    created_at       timestamp        default CURRENT_TIMESTAMP,
-    last_modified_at timestamp        default CURRENT_TIMESTAMP,
-    user_id          bigint,
-    content          text,
-    view_count       integer not null default 0,
-    vaccination_type varchar(255),
-    primary key (post_id)
-);
-
-create table comment
-(
-    comment_id       bigint auto_increment not null,
-    created_at       timestamp default CURRENT_TIMESTAMP,
-    last_modified_at timestamp default CURRENT_TIMESTAMP,
-    post_id          bigint,
-    user_id          bigint,
-    content          text,
-    primary key (comment_id)
-);
-
-create table likes
-(
-    likes_id         bigint auto_increment not null,
-    created_at       timestamp default CURRENT_TIMESTAMP,
-    last_modified_at timestamp default CURRENT_TIMESTAMP,
-    post_id          bigint,
-    user_id          bigint,
-    primary key (likes_id)
-);
-
-alter table post
-    add constraint fk_post_user foreign key (user_id) references user (user_id) on delete cascade;
-alter table comment
-    add constraint fk_comment_user foreign key (user_id) references user (user_id) on delete cascade;
-alter table comment
-    add constraint fk_comment_post foreign key (post_id) references post (post_id) on delete cascade;
-alter table likes
-    add constraint fk_likes_user foreign key (user_id) references user (user_id) on delete cascade;
-alter table likes
-    add constraint fk_likes_post foreign key (post_id) references post (post_id) on delete cascade;
-
-
+drop table if exists vaccination_statistic;
+drop table if exists public_data;
 
 create table public_data
 (
@@ -83,6 +25,19 @@ create table vaccination_statistic
     vaccination_statistic_id bigint not null,
     primary key (vaccination_statistic_id)
 );
+
+alter table comment
+    add last_modified_at timestamp default CURRENT_TIMESTAMP;
+alter table comment
+    modify column content text;
+alter table likes
+    add last_modified_at timestamp default CURRENT_TIMESTAMP;
+alter table user
+    add last_modified_at timestamp default CURRENT_TIMESTAMP;
+alter table post
+    add last_modified_at timestamp default CURRENT_TIMESTAMP;
+alter table post
+    modify column content text;
 
 alter table vaccination_statistic
     add constraint fk_vaccination_statistic_public_data foreign key (vaccination_statistic_id) references public_data (public_data_id) on delete cascade on update cascade;
