8b84140a92bb411e4da5354f19f06e34652523ba
youngbin <thisisyoungbin@gmail.com>
2021-09-29 15:11:21 +0900

    refactor: 멀티 모듈 yml 파일 설정

--- backend/app-cvi-api/src/main/java/com/cvi/service/EnvSecret.java
+++ backend/app-cvi-api/src/main/java/com/cvi/service/EnvSecret.java
@@ -1,10 +1,11 @@
 package com.cvi.service;
 
-import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Slf4j
 @Component
@@ -17,5 +18,6 @@ public class EnvSecret {
     public void init() {
         log.info("naver.client-secret = " + env.getProperty("security.auth.naver.client-secret"));
         log.info("jwt.secret-key = " + env.getProperty("security.jwt.token.secret-key"));
+        log.info("inbi.handsome = " + env.getProperty("inbi"));
     }
 }
