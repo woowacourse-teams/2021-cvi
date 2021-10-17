5e6b9c4278e11c5ce8b9e3502ba497c939f61977
livenow14 <xowjd41@naver.com>
2021-10-17 12:40:43 +0900

    feat: 스케쥴러 동작 재구성 및 동시성 문제 해결
    동시성 이슈가 걸리는 곳에 Shcedule 도메인의 낙관적 락을 사용함
    
    현재 추가 refactoring이 필요함.

---
+++ backend/domain-cvi-scheduler/src/main/java/com/cvi/service/scheduler/ScheduledTasks.java
@@ -0,0 +1,54 @@
+package com.cvi.service.scheduler;
+
+import com.cvi.service.PublicDataService;
+import com.cvi.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+@Profile("!test")
+@RequiredArgsConstructor
+public class ScheduledTasks {
+
+    private static final String PUBLIC_DATA = "Public Data";
+
+    private final PublicDataService publicDataService;
+    private final ScheduleService scheduleService;
+
+    @PostConstruct
+    private void initializeVaccinationDate() {
+        scheduleService.saveSchedule(PUBLIC_DATA);
+/*        LocalDate today = LocalDate.now();
+        log.info("한국 백신접종률 api요청 및 저장 시작:");
+        publicDataService.saveVaccinationStatistics(today);
+        log.info("한국 백신접종률 api요청 완료 및 데이터베이스 저장 완료");*/
+    }
+
+    @Scheduled(cron = "0 0/1 0/1 * * ?")
+    private void scheduleKoreaVaccinationData2() {
+        scheduleService.activeSchedule(PUBLIC_DATA,
+                () -> {
+                    LocalDate today = LocalDate.now();
+                    log.info("[스케쥴러] 한국 백신접종률 api요청 및 저장 시작:");
+                    publicDataService.saveVaccinationStatistics(today);
+                    log.info("[스케쥴러] 한국 백신접종률 api요청 완료 및 데이터베이스 저장 완료");
+                }
+        );
+    }
+
+    @Scheduled(cron = "0 20 05 * * ?")
+    private void scheduleWorldVaccinationData() {
+        LocalDateTime today = LocalDateTime.now();
+        log.info("[스케쥴러] 세계 백신접종률 api요청 및 저장 시작. 시간: {}", today);
+        publicDataService.saveWorldVaccinationStatistics(today.toLocalDate());
+        log.info("[스케쥴러] 세계 api요청 완료 및 데이터베이스 저장 완료. 시간: {}", today);
+    }
+}
