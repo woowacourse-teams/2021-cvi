0c7190f6a754d391fed42927036c473d0f01d06a
jum0 <wnsah052@naver.com>
2021-10-27 21:40:02 +0900

    test: beforeLogin 테스트 작성

--- frontend/cypress/pages/beforeLogin.test.js
+++ frontend/cypress/pages/beforeLogin.test.js
@@ -1,108 +1,148 @@
-import { BASE_URL } from '../../src/constants';
+import { ALERT_MESSAGE, PAGING_SIZE } from '../../src/constants';
 
 describe('Before Login Test', () => {
-  const getReviewList = () => {
-    cy.intercept('GET', `${BASE_URL}/posts`, { fixture: 'reviewList' }).as('requestReviewData');
+  const getTheLatestPreviewList = () => {
+    cy.intercept(
+      'GET',
+      `https://dev.cvi-korea.r-e.kr/api/v1/posts/paging?offset=0&size=${PAGING_SIZE}&sort=CREATED_AT_DESC`,
+      {
+        fixture: 'theLatestPreviewList',
+      },
+    ).as('theLatestPreviewList');
+  };
+
+  const getTheMostFamousPreviewList = () => {
+    cy.intercept(
+      'GET',
+      `https://dev.cvi-korea.r-e.kr/api/v1/posts/paging?offset=0&size=${PAGING_SIZE}&sort=LIKE_COUNT_DESC`,
+      {
+        fixture: 'theMostFamousPreviewList',
+      },
+    ).as('theMostFamousPreviewList');
   };
 
   before(() => {
-    getReviewList();
+    getTheLatestPreviewList();
+    getTheMostFamousPreviewList();
 
     cy.visit('');
-    // cy.wait('@requestReviewData');
-  });
-
-  it.skip('홈페이지에 접속하여 접종 현황을 본다.', () => {
+    cy.wait('@theLatestPreviewList');
+    cy.wait('@theMostFamousPreviewList');
     cy.waitForReact();
-    // cy.react('VaccinationState', { props: { title: '접종 현황' }, exact: true }).contains(
-    //   '접종 현황',
-    // );
   });
 
-  it.skip('홈페이지에 접속하여 접종 후기를 본다.', () => {
-    getReviewList();
-
-    cy.visit('');
-    cy.wait('@requestReviewData');
+  // 홈페이지 컴포넌트 확인
+  it('홈페이지에 접속하여 접종 현황을 본다.', () => {
+    cy.react('VaccinationState', { props: { title: '접종 현황' } }).should('exist');
   });
 
-  it.skip('홈페이지에 있는 첫 번째 접종 후기를 클릭하면, 첫 번째 접종 후기의 접종 상세 페이지가 보인다.', () => {
-    cy.get('ul li:first').click();
+  it('홈페이지에 접속하여 최신 글을 본다.', () => {
+    cy.react('Preview', { props: { title: '최신 글' } }).should('exist');
+  });
+
+  it('홈페이지에 접속하여 실시간 인기 글을 본다.', () => {
+    cy.react('Preview', { props: { title: '실시간 인기 글' } }).should('exist');
+  });
+
+  it('홈페이지에 접속하여 접종 예약 하러가기 버튼을 본다', () => {
+    cy.contains('a', '접종 예약 하러가기').should('exist');
+  });
+
+  it('홈페이지에 접속하여 접종 후기 쓰러가기 버튼을 본다', () => {
+    cy.react('Button').contains('접종 후기 쓰러가기').should('exist');
+  });
+
+  it('홈페이지에 접속하여 잔여 백신 보러가기 버튼을 본다', () => {
+    cy.contains('a', '잔여 백신 보러가기').should('exist');
+  });
+
+  // + 새로운 창 열리는 것 확인하는 테스트 코드
+
+  // warning 발생
+  // stub 필요
+  it.skip('홈페이지에 있는 최신 글의 첫 번째 후기를 클릭하면, 최신 글 첫 번째 후기의 상세 페이지가 보인다.', () => {
+    cy.react('Preview', { props: { title: '최신 글' } })
+      .find('ul li:first')
+      .click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).contains('/review/');
+    });
+
+    cy.contains('a', '홈').click();
+  });
+
+  it('홈페이지에 있는 실시간 인기 글의 첫 번째 후기를 클릭하면, 실시간 인기 글 첫 번째 후기의 상세 페이지가 보인다.', () => {
+    cy.react('Preview', { props: { title: '실시간 인기 글' } })
+      .find('ul li:first')
+      .click();
 
     cy.location().should((loc) => {
       expect(loc.pathname).contains('/review/');
     });
   });
 
-  it.skip('목록 보기 버튼을 클릭하면, 전체 접종 후기 페이지가 보인다.', () => {
+  it('목록 보기 버튼을 클릭하면, 접종 후기 페이지가 보인다.', () => {
     cy.contains('div', '목록 보기').click();
 
-    cy.location().should((loc) => {
-      expect(loc.pathname).contains('/review');
-    });
+    cy.location('pathname', { timeout: 4000 }).should('equal', '/review');
   });
 
-  it.skip('모더나 탭을 클릭하면, 모더나 접종 후기만 보인다.', () => {
-    cy.contains('button', '모더나').click();
+  // 클릭했는데, 렌더링이 안되는 경우도 있는 것 같기도 하고..
+  it('모더나 탭을 클릭하면, 모더나 접종 후기만 보인다.', () => {
+    cy.react('Button').contains('모더나').click();
+    // cy.contains('button', '모더나').click();
 
-    cy.waitForReact();
-    cy.react('Label').contains('화이자').should('have.length', '0');
-    cy.react('Label').contains('얀센').should('have.length', '0');
-    cy.react('Label').contains('아스트라제네카').should('have.length', '0');
+    // cy.waitForReact();
+    cy.react('Label').contains('화이자').should('not.exist');
+    cy.react('Label').contains('얀센').should('not.exist');
+    cy.react('Label').contains('아스트라제네카').should('not.exist');
   });
 
   it.skip('화이자 탭을 클릭하면, 화이자 접종 후기만 보인다.', () => {
     cy.contains('button', '화이자').click();
 
-    cy.waitForReact();
-    cy.react('Label').contains('모더나').should('have.length', '0');
-    cy.react('Label').contains('얀센').should('have.length', '0');
-    cy.react('Label').contains('아스트라제네카').should('have.length', '0');
+    cy.react('Label').contains('모더나').should('not.exist');
+    cy.react('Label').contains('얀센').should('not.exist');
+    cy.react('Label').contains('아스트라제네카').should('not.exist');
   });
 
   it.skip('얀센 탭을 클릭하면, 얀센 접종 후기만 보인다.', () => {
     cy.contains('button', '얀센').click();
 
-    cy.waitForReact();
-    cy.react('Label').contains('화이자').should('have.length', '0');
-    cy.react('Label').contains('모더나').should('have.length', '0');
-    cy.react('Label').contains('아스트라제네카').should('have.length', '0');
+    cy.react('Label').contains('화이자').should('not.exist');
+    cy.react('Label').contains('모더나').should('not.exist');
+    cy.react('Label').contains('아스트라제네카').should('not.exist');
   });
 
   it.skip('아스트라제네카 탭을 클릭하면, 아스트라제네카 접종 후기만 보인다.', () => {
     cy.contains('button', '아스트라제네카').click();
 
-    cy.waitForReact();
-    cy.react('Label').contains('화이자').should('have.length', '0');
-    cy.react('Label').contains('모더나').should('have.length', '0');
-    cy.react('Label').contains('얀센').should('have.length', '0');
+    cy.react('Label').contains('화이자').should('not.exist');
+    cy.react('Label').contains('모더나').should('not.exist');
+    cy.react('Label').contains('얀센').should('not.exist');
     expect();
   });
 
-  it.skip('후기 작성 버튼을 클릭하면, 로그인을 안내하는 alert가 보인다.', () => {
-    cy.contains('button', '후기 작성').click();
+  it.skip('후기 작성 버튼을 누르면 로그인을 안내하는 alert가 보이고, 취소 버튼을 클릭한다.', () => {
+    cy.react('Button').contains('후기 작성').click();
 
     cy.on('window:confirm', (text) => {
-      expect(text).to.equal('로그인이 필요한 서비스입니다.');
+      expect(text).to.equal(ALERT_MESSAGE.NEED_LOGIN);
     });
     cy.on('window:confirm', () => false);
   });
 
-  it.skip('취소 버튼을 클릭한다.', () => {
-    cy.contains('button', '후기 작성').click();
-
-    cy.on('window:confirm', () => false);
+  it.skip('접종 후기 페이지가 보인다', () => {
+    cy.location('pathname').should('equal', '/review');
   });
 
-  it.skip('다시 후기 작성 버튼을 누르고, alert에서 확인 버튼을 클릭한다.', () => {
+  it.skip('후기 작성 버튼을 누르고 확인을 누르면, 로그인 페이지가 보인다', () => {
     cy.contains('button', '후기 작성').click();
 
     cy.on('window:confirm', () => true);
   });
 
   it.skip('로그인 페이지가 보인다.', () => {
-    cy.location().should((loc) => {
-      expect(loc.pathname).contains('/login');
-    });
+    cy.location('pathname').should('equal', '/login');
   });
 });
