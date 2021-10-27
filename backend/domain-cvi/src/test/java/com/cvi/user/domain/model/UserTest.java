cd7401c360fc9c0188167a1bc8541e931a4ab003
younghoonkwon <younghoon.kwon91@gmail.com>
2021-10-28 01:39:51 +0900

    refactor: UserTest update 케이스 추가

--- backend/domain-cvi/src/test/java/com/cvi/user/domain/model/UserTest.java
+++ backend/domain-cvi/src/test/java/com/cvi/user/domain/model/UserTest.java
@@ -4,10 +4,13 @@ import com.cvi.CustomParameterizedTest;
 import com.cvi.exception.InvalidInputException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -65,20 +68,27 @@ class UserTest {
 
     @DisplayName("사용자 수정 - 성공")
     @CustomParameterizedTest
-    @ValueSource(strings = {"ㅁㅇㄹㅁㅇㄹ", "ㅓㅓㅓㅓ", "adfdf", "검프", "검프23213"})
-    void update(String name) {
+    @MethodSource
+    void update(String name, AgeRange ageRange, boolean shotVerified) {
         //given
         User updateUser = User.builder()
                 .id(1L)
-                .ageRange(AgeRange.TWENTIES)
+                .ageRange(ageRange)
                 .createdAt(LocalDateTime.now())
                 .nickname(name)
                 .profileUrl("www.budae.com")
+                .shotVerified(shotVerified)
                 .socialProvider(SocialProvider.KAKAO)
                 .build();
         //when
         user.update(updateUser);
         //then
         assertThat(user.getNickname()).isEqualTo(updateUser.getNickname());
+        assertThat(user.getAgeRange()).isEqualTo(updateUser.getAgeRange());
+        assertThat(user.isShotVerified()).isEqualTo(updateUser.isShotVerified());
+    }
+
+    static Stream<Arguments> update(){
+        return Stream.of(Arguments.of("1", AgeRange.TWENTIES, true), Arguments.of("2", AgeRange.THIRTIES, false));
     }
 }
